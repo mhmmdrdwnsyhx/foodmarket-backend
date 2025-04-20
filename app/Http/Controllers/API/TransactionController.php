c3b412e34a6c73be74f5986764ea5b90be7cad13
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-04-20 14:56:55 +0700

    add MidtransController & Update routes API

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class TransactionController extends Controller
 {
@@ -52,15 +54,76 @@ class TransactionController extends Controller
     }
 
     // Transaction API Update for Testing
-    // public function update(Request $request, $id)
-    // {
-    //     $transaction = Transaction::findOrFail($id);
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::findOrFail($id);
+        
+        $transaction->update($request->all());
 
-    //     $transaction->update($request->all());
+        return ResponseFormatter::success(
+            $transaction,
+            'Data transaksi berhasil diupdate'
+        );
+    }
 
-    //     return ResponseFormatter::success(
-    //         $transaction,
-    //         'Data transaksi berhasil diupdate'
-    //     );
-    // }
+    // Transaction API Checkout
+    public function checkout (Request $request)
+    {
+        $request->validate([
+            'food_id' => 'required|exists:food,id',
+            'user_id' => 'required|exists:users,id',
+            'quantity' => 'required',
+            'total' => 'required',
+            'status' => 'required',
+        ]);
+
+        $transaction = Transaction::create([
+            'food_id' => $request->food_id,
+            'user_id' => $request->user_id,
+            'quantity' => $request->quantity,
+            'total' => $request->total,
+            'status' => $request->status,
+            'payment_url' => '',
+        ]);
+
+        //Midtrans Transaction
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        //Calling Midtrans Transaction
+        $transaction = Transaction::with(['food', 'user'])->find($transaction->id);
+
+        //Midtrans Configuration
+        $midtrans = [
+            'transaction_details' => [
+                'order_id' => $transaction->id,
+                'gross_amount' => (int) $transaction->total,
+            ],
+            'customer_details' => [
+                'first_name' => $transaction->user->name,
+                'email' => $transaction->user->email,
+            ],
+            'enabled_payments' => ['gopay', 'bank_transfer'],
+            'vtweb' => []
+        ];
+
+        //Calling Midtrans API
+        try {
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+            
+            $transaction->payment_url = $paymentUrl;
+            $transaction->save();
+
+            //Retrieving Midtrans Data to API
+            return ResponseFormatter::success(
+                $transaction,
+                'Transaksi berhasil'
+            );
+
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 'Transaksi gagal');
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\FoodController;
 use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\MidtransController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,8 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('user/photo', [UserController::class, 'updatePhoto']);
     Route::post('logout', [UserController::class, 'logout']);
 
+    Route::post('checkout', [TransactionController::class, 'checkout']);
+
     Route::get('transaction', [TransactionController::class, 'all']);
     Route::post('transaction/{id}', [TransactionController::class, 'update']);
 });
@@ -31,3 +34,5 @@ Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 
 Route::get('food', [FoodController::class, 'all']);
+
+Route::post('midtrans/callback', [MidtransController::class, 'callback']);
